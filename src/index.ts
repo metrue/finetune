8d67ed020fece18c3b1601c2c138d125ce5237e8
Minghe Huang <h.minghe@gmail.com>
2023-09-01 13:19:26 +0800

    update

---
+++ src/index.ts
@@ -0,0 +1,97 @@
+import OpenAI from 'openai'
+import core from '@actions/core'
+import fs from 'fs'
+//const github = require('@actions/github')
+
+const sleep = (delay: number) => new Promise((resolve) => setTimeout(resolve, delay))
+
+const untilFilesProcessed = async (openai: OpenAI, fileId: string): Promise<boolean> => {
+  let fileProcessed = false
+  const res = await openai.files.list()
+  for (const f of res.data) {
+    const { status, id, purpose } = f
+    // eslint-disable-next-line
+    console.log(`${id} ${purpose} ${status}`)
+    if (id === fileId && status !== 'processed') {
+      fileProcessed = true
+    }
+  }
+
+  if (!fileProcessed) {
+    await sleep(30000)
+    return untilFilesProcessed(openai, fileId)
+  }
+  return true
+}
+
+const untilFineTuningJobCompleted = async (openai: OpenAI, jobId: string): Promise<string> => {
+  let jobCompleted = false
+  let modelTrained = ''
+  const res = await openai.fineTunes.list()
+  for (const job of res.data) {
+    const { id, status, model } = job
+    if (id === jobId && status == 'succeeded`') {
+      jobCompleted = true
+      modelTrained = model
+    }
+  }
+
+  if (!jobCompleted) {
+    await sleep(30000)
+    return untilFineTuningJobCompleted(openai, jobId)
+  }
+  return modelTrained
+}
+
+const main = async () => {
+  // `who-to-greet` input defined in action metadata file
+  const dataset = core.getInput('dataset')
+  const OPENAI_API_KEY = core.getInput('openai-api-key')
+  const OPENAI_API_ORG = core.getInput('openai-api-org')
+  // eslint-disable-next-line
+  const openai = new OpenAI({
+    apiKey: OPENAI_API_KEY,
+    dangerouslyAllowBrowser: true,
+    organization: OPENAI_API_ORG,
+  })
+
+  const fd = fs.createReadStream(dataset, 'utf8')
+  const res = await openai.files.create(
+    {
+      file: fd,
+      purpose: 'fine-tune',
+    },
+    {
+      stream: true,
+    },
+  )
+
+  // eslint-disable-next-line
+  console.log(`dataset uploaded: ${res.filename} ${res.id} ${res.status}`)
+  if (res.id) {
+    await untilFilesProcessed(openai, res.id)
+  } else {
+    throw new Error(`no id found for the dataset uploaded`)
+  }
+
+  const job = await openai.fineTuning.jobs.create({
+    model: 'gpt-3.5-turbo',
+    training_file: res.id,
+  })
+
+  if (job.id) {
+    const model = await untilFineTuningJobCompleted(openai, job.id)
+    core.setOutput('model', model)
+  } else {
+    throw new Error(`no id found for the fine tuning job`)
+  }
+}
+
+main()
+  .then((data) => {
+    // eslint-disable-next-line
+    console.log(data)
+  })
+  .catch((e) => {
+    core.setFailed(e.message)
+  })
